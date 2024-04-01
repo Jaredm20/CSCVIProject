f8c71682281bfd4626a69931f6721746a7894d2e
Jaredm20 <aurier256@gmail.com>
2024-03-31 20:39:04 -0500

    rewrite

--- server/server.js
+++ server/server.js
@@ -4,12 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    console.log('Request URL:', req.url);
-    next();
-});
-
-app.use(express.static(path.join(__dirname, '../JST/JST/myapp/public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
